85238f1592299454c8aa3be6ec54e0573d313c3a
Sherif <4chimabika@gmail.com>
2025-01-19 11:07:36 +0100

    feat(setup): Updated the project setup to enable tailwind css and other configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  // content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
